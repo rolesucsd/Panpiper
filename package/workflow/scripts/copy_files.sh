ca14e5099cc7d68d1e4579bbfe33648a3b877ac0
Renee Oles <roles@health.ucsd.edu>
2023-01-16 10:40:06 -0800

    update quality

--- package/workflow/scripts/copy_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir resources/fasta
-while IFS= read -r line
-do
-    cp "results/Assembly/{$line}/{$line.fna}" "resources/fasta/{$line}.fna"
-done < results/Quality/sample_list.txt
